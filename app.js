1b881323145cee3e470682f9bba97c207db0d60d
Irene_Liou <brtm55648@gmail.com>
2021-08-02 21:44:54 +0800

    feat: modify index

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const routes = require('./routes')
 
 const app = express()
 const port = 3000
@@ -8,6 +9,8 @@ require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(routes)
 
 app.get('/', (req, res) => {
   res.render('index')
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const home = require('./modules/home')
+const records = require('./modules/records')
+
+router.use('/', home)
+
+module.exports = router
