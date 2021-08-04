f1f4e367d6cbca9c9cae71079cfaa59690b003f6
Irene_Liou <brtm55648@gmail.com>
2021-08-05 00:17:42 +0800

    feat: add handlebaraHelper

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const routes = require('./routes')
+const ifEqual = require('./tools/handlebarshelpers')
 
 const app = express()
 const port = 3000
 
 require('./config/mongoose')
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  helpers: { ifEqual }
+}))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(routes)
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+
 
 app.listen(port, () => {
   console.log(`This is listening on http://localhost:${port}`)
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
+const Category = require('../../models/category')
 
-router.get('/', (req, res) => {
-  Record.find()
-    .lean()
-    .then(records => res.render('index', { records }))
-    .catch(error => console.error(error))
+router.get('/', async (req, res) => {
+  const categoryList = await Category.find().sort({ _id: 'asc' }).lean()
+  const records = await Record.find().lean().sort({ date: 'desc', _id: 'desc' })
+  let totalAmount = 0
+  for (let record of records) {
+    totalAmount += record.amount
+  }
+  res.render('index', { totalAmount, records, categoryList })
 })
 
 module.exports = router
