5f172b9691eb2f0d89c3c18fe3cfdc05706da594
Irene_Liou <brtm55648@gmail.com>
2021-08-08 21:54:07 +0800

    feat: add edit function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 
+//新增
 router.get('/new', (req, res) => {
   Category.find()
     .lean()
@@ -18,5 +19,29 @@ router.post('/', (req, res) => {
     .catch((error) => console.log(error))
 })
 
+//編輯
+router.get('/:id/edit', async (req, res) => {
+  const id = req.params.id
+  const categoryList = await Category.find().sort({ _id: 'asc' }).lean()
+  return Record.findById(id)
+    .lean()
+    .then((record) => res.render('edit', { record, categoryList }))
+    .catch((error) => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const { name, date, category, amount } = req.body
+  return Record.findById(id)
+    .then((record) => {
+      record.name = name
+      record.date = date
+      record.category = category
+      record.amount = amount
+      return record.save()
+    })
+    .then(() => { res.redirect('/') })
+    .catch((error) => console.log(error))
+})
 
 module.exports = router
