9dae463a2777058ddaa64833b42d961af9ad1f72
Irene_Liou <brtm55648@gmail.com>
2021-08-10 22:00:48 +0800

    feat: add delete function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -44,4 +44,13 @@ router.put('/:id', (req, res) => {
     .catch((error) => console.log(error))
 })
 
+//刪除
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => { res.redirect('/') })
+    .catch((error) => console.log(error))
+})
+
 module.exports = router
